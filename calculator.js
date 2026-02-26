149b5383e8c12ae4f6f7355805fe373a8ba42b8f
Nikhil Kulkarni <student.nikhilkulkarni@gmail.com>
2026-02-26 15:49:34 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function clearDisplay(){
+    document.getElementById("display").value =" ";
+}
+
+function appendToDisplay(value){
+    document.getElementById("display").value += value;
+}
+
+function calculateResult(){
+    var display = document.getElementById("display");
+    try{
+        display.value = eval(display.value);
+    } catch(error){
+        display.value ="Error";
+    }
+}
